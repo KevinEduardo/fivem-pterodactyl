7aa91f79616f3e0ec6a81fa27ded1ac5bf70fe91
Avery Johnson <avery627@gmail.com>
2017-12-04 00:43:42 -0500

    Updated FiveM Version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ sleep 2
 cd /home/container
 
 # Download FiveM Server Files
-wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/347-2bfc2c900e2ae2246ed3e4a7caccadddbbbfd2c6/fx.tar.xz
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/431-d055d83842b6391e88381fb13c421ee5f1811185/fx.tar.xz
 tar xf fx.tar.xz
 rm fx.tar.xz
 
