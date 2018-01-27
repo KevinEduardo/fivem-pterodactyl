af4ae7242449621b002954424fc9899b26f1a37c
Kevin Eduardo <KevinEduardo@users.noreply.github.com>
2018-01-27 02:04:40 -0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ sleep 2
 cd /home/container
 
 # Download FiveM Server Files
-wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/431-d055d83842b6391e88381fb13c421ee5f1811185/fx.tar.xz
+# wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/431-d055d83842b6391e88381fb13c421ee5f1811185/fx.tar.xz
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/446-8a058b67e5f11677c380a330f677e768e2b2a705/fx.tar.xz
 tar xf fx.tar.xz
 rm fx.tar.xz
 
