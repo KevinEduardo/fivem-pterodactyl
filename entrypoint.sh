165f5b723ff29b5fa959cb9c524344df04f3bb95
Kevin Eduardo <KevinEduardo@users.noreply.github.com>
2018-01-27 02:39:35 -0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,12 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "${MODIFIED_STARTUP}"
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
